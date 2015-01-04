7c6951407ab29eb9dc5a7743f82bd876b1a92301
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2015-01-04 13:31:22 +0100

    Add volume persistence for data

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
-docker run -d --name elasticsearch -h elasticsearch schabrolles/elasticsearch:1.4
-#docker run -ti --name elasticsearch -h elasticsearch schabrolles/elasticsearch:1.4 /bin/bash
+docker run -d --name elasticsearch -h elasticsearch -v /var/lib/elasticsearch:/var/lib/elasticsearch schabrolles/elasticsearch:1.4
+#docker run -ti --name elasticsearch -h elasticsearch -v /var/lib/elasticsearch:/var/lib/elasticsearch schabrolles/elasticsearch:1.4 /bin/bash
 
