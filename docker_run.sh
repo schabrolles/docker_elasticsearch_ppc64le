36d57dfede88baa95a5285e309814376598f829e
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2015-01-05 12:40:50 +0100

    Add peristent mount from host for logs/conf files

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
-docker run -d --name elasticsearch -h elasticsearch -v /var/lib/elasticsearch:/var/lib/elasticsearch schabrolles/elasticsearch:1.4
+docker run -d --name elasticsearch -h elasticsearch -v /var/lib/elasticsearch:/var/lib/elasticsearch -v /var/log/elasticsearch:/var/log/elasticsearch -v /etc/elasticsearch:/etc/elasticsearch schabrolles/elasticsearch:1.4
 #docker run -ti --name elasticsearch -h elasticsearch -v /var/lib/elasticsearch:/var/lib/elasticsearch schabrolles/elasticsearch:1.4 /bin/bash
 
